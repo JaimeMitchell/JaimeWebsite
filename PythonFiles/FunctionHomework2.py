8f1e057cebfb0c95e0953bf9cac184436453f2cd
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-04-01 23:13:25 -0400

    spelling

--- PythonFiles/FunctionHomework2.py
+++
@@ -1,8 +0,0 @@
-import pickle
-with open('STU.pkl','rb') as stud:
-    inputGrade=pickle.load(stud)
-while(1==1):
-    name=input('Which student do you want to check? ')
-    for i in range(0,numStu,1):
-        if (names[i]==name):
-            print(str(name)+"'s grade is "+str(grades[i])+'.')
